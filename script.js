c1aca1a9a58c9e1c45d768905bbb0b343b400f8c
Axel <cymbalaxel@gmail.com>
2022-04-06 15:22:55 -0300

    se agrego funcion operator

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 function add(a, b){
 return a + b;
-}
+};
 
 function multiply(a, b){
 return a * b;
-}
+};
 
 function substract(a, b){
     return a - b;
-}
+};
 
 function divide(a, b){
     return a / b;
+};
+
+function operator(o, n1, n2){
+    if (o == '/'){
+         console.log(divide(n1, n2));
+    }
+    if (o == '+'){
+        console.log(add(n1, n2));
+    }
+    if (o == '-'){
+        console.log(substract(n1, n2));
+    }
+    if (o == '*'){
+        console.log(multiply(n1, n2));
+    }
 }
