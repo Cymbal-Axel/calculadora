04707593f0bfafa4c11b2185468899935ea09cbd
Axel <cymbalaxel@gmail.com>
2022-04-20 15:07:19 -0300

    importe los numeros, operadores y los valores del display

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 var displayValorAnterior = document.getElementById(valorAnterior);
 var displayValorActual = document.getElementById(valorActual);
+var botonesNumeros = document.querySelectorAll('.numeros');
+var botonesOperadores = document.querySelectorAll('.operacion');
+
+
+const calculadora = new Calculadora();
+
