12ad5fa444a850664837174bd0e5e275037086f5
Axel <cymbalaxel@gmail.com>
2022-04-20 21:04:11 -0300

    arregle dos getElementById que les faltaba comillas

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-var displayValorAnterior = document.getElementById(valorAnterior);
-var displayValorActual = document.getElementById(valorActual);
+var displayValorAnterior = document.getElementById('valorAnterior');
+var displayValorActual = document.getElementById('valorActual');
 var botonesNumeros = document.querySelectorAll('.numeros');
 var botonesOperadores = document.querySelectorAll('.operacion');
 
+//creo una instancia de Display y como parametro le paso los valores de display anterior y actual.
+const display = new Display(displayValorAnterior, displayValorActual);
 
-const calculadora = new Calculadora();
-
+botonesNumeros.forEach(boton => {
+    //cada vez que alguien haga click, el display debe agregar un numero.
+    boton.addEventListener('click', () => {
+        //"agregarNumero" es un metodo de la clase Display
+        display.agregarNumero(boton.innerHTML)
+    });
+});
